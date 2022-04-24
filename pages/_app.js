2632fd88ae735a4076b229d58e5134a16677bc9d
Errytagedesign <79514299+Errytagedesign@users.noreply.github.com>
2022-04-24 16:05:45 +0100

    layout components created

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import NProgress from "nprogress";
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head></Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
