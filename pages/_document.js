8ef74a17c4a96923b2bcd667f21d82bf29cc7fe3
MBP <mbp@MBPs-MacBook-Pro.local>
2022-08-03 20:25:53 +0100

    fixes

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=optional"
             rel="stylesheet"
           />
         </Head>
