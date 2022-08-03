67fba2e0f7067165ffa1fcbe8589380a29f032b9
MBP <mbp@MBPs-MacBook-Pro.local>
2022-08-03 20:19:52 +0100

    fixes

--- components/searchpage.style.js
+++ components/searchpage.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 1em auto;
-
+  font-family: "Ubuntu", sans-serif;
   /*width: 100%;
   display: flex;
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 // highlighs next line
 
@@ -29,5 +29,23 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
